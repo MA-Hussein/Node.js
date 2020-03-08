f9916fc809f96b3204d7e288d68aa46248ccb222
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-03-08 21:51:02 +0100

    Update app.js

--- week2/Homework/exercise-1/app.js
+++ week2/Homework/exercise-1/app.js
@@ -21,7 +21,25 @@ app.put('/blogs', (req, res) => {
     res.end('post does not exist');
   }
 });
+app.delete('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  if (fs.existsSync(title)) {
+    fs.unlinkSync(title);
+    res.end('ok');
+  } else {
+    res.end('post does not exist');
+  }
+});
 
+app.get('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+
+  if (fs.existsSync(title)) {
+    res.sendfile(title);
+  } else {
+    res.end('post does not exist');
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server started at port: ', PORT));
